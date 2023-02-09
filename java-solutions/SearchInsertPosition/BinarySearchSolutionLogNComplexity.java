4cc45f0d27fa94240d17e7a569e5e120a6d52282
Jakub Łągwa <kklagwa@gmail.com>
2023-02-09 23:41:33 +0100

    beats 100 procent time complexity

---
+++ java-solutions/SearchInsertPosition/BinarySearchSolutionLogNComplexity.java
@@ -0,0 +1,26 @@
+package SearchInsertPosition;
+
+
+//this code has O(log n) complexity
+//im więcej danych wejściowych, tym proporcjonalnie 
+//mniej operacji program musi wykonać w porównaniu
+//z ilością danych
+public class BinarySearchSolutionLogNComplexity {
+	public int searchInsert(int[] nums, int target) {
+		int left=0;
+		int right=nums.length-1;
+		
+		while(left<=right) {
+			int mid=left+(right-left)/2;
+			if(nums[mid]==target) {
+				return mid;
+			}else if(nums[mid]<target) {
+				left=mid+1;
+			}else {
+				right=mid-1;
+			}
+		}
+		return left;
+	}
+	//correct, beats 100% time complexity
+}
--- java-solutions/SearchInsertPosition/Solution1CorrectTimeLimitExceeded.java
+++ java-solutions/SearchInsertPosition/Solution1CorrectTimeLimitExceeded.java
@@ -3,6 +3,13 @@ package SearchInsertPosition;
 
 //wrong: required algorithm with O(log n) runtime 
 //complexity
+
+//this code time complexity is liniowa O(n)
+//w najgorszym wypadku - w przypadku przechodzenia 
+//przez wszystkie elementy złożoność będzie 
+//wynosiła nxC gdzie n rozmiar tablicy a C to stała
+//reprezentująca czas potrzebny na porównanie każdego
+//elementu
 class Solution1CorrectTimeLimitExceeded {
     public int searchInsert(int[] nums, int target) {
     	int i=0;
